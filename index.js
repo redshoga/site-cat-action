a5051fb6632ddea84eb6e091a0d60b73318ef8ff
Yui Ichioka <ichioka.yui@fixer.co.jp>
2019-12-17 19:13:11 +0900

    0.22

--- index.js
+++ index.js
@@ -2,33 +2,46 @@ const core = require('@actions/core');
 const fs = require('fs');
 const Octokit = require("@octokit/rest");
 
+const isLgtmString = (target) => {
+  return target.trim().toLowerCase() === "lgtm"
+}
+
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    console.log(process.env)
+
     // action info
     const repoOwner = process.env.GITHUB_REPOSITORY.split("/")[0]
     const repoName = process.env.GITHUB_REPOSITORY.split("/")[1]
-    if (!["issue_comment"].includes(process.env.GITHUB_EVENT_NAME)) return;
-
-    // issue info
-    const issueInfo = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
-    if (issueInfo.comment.body !== "LGTM") return
+    // if (!["issue_comment"].includes(process.env.GITHUB_EVENT_NAME)) return;
 
     // github client
     const token = core.getInput('token');
-    console.log("token:", token)
-
     const octokit = new Octokit({
       auth: `token ${token}`
     });
 
-    // comment site-cat
-    octokit.issues.createComment({
-      owner: repoOwner,
-      repo: repoName,
-      issue_number: issueInfo.issue.number,
-      body: "![site-cat](https://user-images.githubusercontent.com/33852683/70984291-8a759d80-20fd-11ea-9e0d-35b559adde8b.jpeg)"
-    })
+    // event info
+    const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+    console.log(event)
+
+    switch (process.env.GITHUB_EVENT_NAME) {
+      case 'issue_comment':
+        if (!isLgtmString(event.comment.body)) return
+
+        octokit.issues.createComment({
+          owner: repoOwner,
+          repo: repoName,
+          issue_number: event.issue.number,
+          body: "![site-cat](https://user-images.githubusercontent.com/33852683/70984291-8a759d80-20fd-11ea-9e0d-35b559adde8b.jpeg)"
+        })
+
+        break;
+      case '...':
+        break;
+    }
+
   }
   catch (error) {
     core.setFailed(error.message);
