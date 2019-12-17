f9f21eb6eda9e2e1037918d190d0300c04364b89
Yui Ichioka <ichioka.yui@fixer.co.jp>
2019-12-17 18:21:28 +0900

    GITHUB_EVENT_PATHをみてみる

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ async function run() {
     core.setOutput("Hello, World!");
     console.log("Hello, World! on console.log()")
     console.log(process.env);
+    const a = require(process.env.GITHUB_EVENT_PATH)
+    console.log("process.env.GITHUB_EVENT_PATH", process.env.GITHUB_EVENT_PATH)
+    console.log(a)
   }
   catch (error) {
     core.setFailed(error.message);
